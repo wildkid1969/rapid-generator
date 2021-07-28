d9291e14daf4550cb922325a21bc5080517926e9
mengya <mengya@aixuexi.com>
2021-07-28 15:53:25 +0800

    add lombok and author and date

--- template/cncounter-manage/src/main/java/${basepackage_dir}/${subpackage_dir}/model/${className}.java
+++ template/cncounter-manage/src/main/java/${basepackage_dir}/${subpackage_dir}/model/${className}.java
@@ -6,21 +6,31 @@ package ${basepackage}.${subpackage}.model;
 
 import java.io.Serializable;
 import java.util.Date;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * ${table.tableAlias}
- * @version 1.0
- * @author
+ * @author: mengya
+ * @Date: ${sysdate}
  */
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @Data
+@ApiModel("${table.tableAlias}")
 public class ${className} implements Serializable {
 
 	<#list table.columns as column>
-	// ${column.columnAlias}
-	private ${column.simpleJavaType} ${column.columnNameLower};
+		/**
+		 * ${column.columnAlias}
+		 */
+		@ApiModelProperty(value = "${column.columnAlias}", dataType = "${column.javaType}")
+		private ${column.simpleJavaType} ${column.columnNameLower};
 	</#list>
 
 }
